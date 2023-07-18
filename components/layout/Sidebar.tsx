893a7ebd24aca72621d8ef3a60ff7105b2b057f4
alptugnamigur <alptugnamigur@gmail.com>
2023-07-18 15:44:25 +0300

    change and creating components and pages

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,44 +1,46 @@
-import { BsBellFill, BsHouseFill } from "react-icons/bs";
-import { FaUser } from "react-icons/fa";
 import { BiLogOut } from "react-icons/bi";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
 
-import SidebarLogo from "./SidebarLogo";
 import SidebarItem from "./SidebarItem";
+import SidebarLogo from "./SidebarLogo";
 import SidebarTweetButton from "./SidebarTweetButton";
 
 const Sidebar = () => {
   const items = [
     {
+      icon: BsHouseFill,
       label: "Home",
       href: "/",
-      icon: BsHouseFill,
     },
     {
+      icon: BsBellFill,
       label: "Notifications",
       href: "/notifications",
-      icon: BsBellFill,
+      auth: true,
     },
     {
-      label: "Profile",
-      href: "/users/123",
       icon: FaUser,
+      label: "Profile",
+      auth: true,
     },
   ];
 
   return (
-    <div className=" col-span-1 h-full pr-4 md:pr-6">
-      <div className=" flex flex-col items-end">
-        <div className=" space-y-2 lg:w-[230px]">
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
           <SidebarLogo />
           {items.map((item) => (
             <SidebarItem
               key={item.href}
+              auth={item.auth}
               href={item.href}
-              label={item.label}
               icon={item.icon}
+              label={item.label}
             />
           ))}
-          <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
+          <SidebarItem icon={BiLogOut} label="Logout" />
           <SidebarTweetButton />
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/Header";
-import Modal from "@/components/Modal";
 
 export default function Home() {
   return (
